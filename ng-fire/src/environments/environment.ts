872581d386a00dd10855edb243bbdcb5a9a1eafb
Stefan Marković <markovicstefan@gmail.com>
2018-03-20 13:55:42 +0100

    Added firebase, material design and core/shared/material modules

---
+++ ng-fire/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAyJIs7CKIsX-di5VhD8VMUn8gyQl1cKUY',
+    authDomain: 'feisty-mechanic-179911.firebaseapp.com',
+    databaseURL: 'https://feisty-mechanic-179911.firebaseio.com',
+    projectId: 'feisty-mechanic-179911',
+    storageBucket: 'feisty-mechanic-179911.appspot.com',
+    messagingSenderId: '319802638019'
+  }
+};
